5a1ed1bc1727f4b8b730c0da4daea644fc55e227
Sergio Vergara <sergiovergara123@gmail.com>
2024-01-25 10:07:17 -0500

    currentDateParagraph element developed inside change event listener. Project completed

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const currentDateParagraph = document.getElementById("current-date");
 const dateOptionsSelectElement = document.getElementById("date-options");
+
 const date = new Date();
 const day = date.getDate();
 const month = date.getMonth() + 1;
@@ -8,4 +9,21 @@ const hours = date.getHours();
 const minutes = date.getMinutes();
 
 const formattedDate = `${day}-${month}-${year}`;
+currentDateParagraph.textContent = formattedDate;
 
+dateOptionsSelectElement.addEventListener("change", () => {
+  switch (dateOptionsSelectElement.value) {
+    case "yyyy-mm-dd":
+      currentDateParagraph.textContent = formattedDate
+        .split("-")
+        .reverse()
+        .join("-");
+      break;
+    case "mm-dd-yyyy-h-mm":
+      currentDateParagraph.textContent = `${month}-${day}-${year} ${hours} Hours ${minutes} Minutes`;
+      break;
+    default:
+      currentDateParagraph.textContent = formattedDate;
+      break;
+  }
+});
